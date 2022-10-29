8158b1b23695e430b8b2a287ad02498312203431
Jenny González <jenny.gonzalez@posteo.de>
2022-10-29 20:32:56 +0200

    upload first files for initial structure

---
+++ rangosApp/src/test/java/com/company/aws/tools/rangos/RangosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.aws.tools.rangos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RangosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
