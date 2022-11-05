a5a0185cbae9066797b30aa42d449406011e3c36
jennycgonzalez <jenny.gonzalez@posteo.de>
2022-11-05 14:07:53 +0100

    Create first model classes to parse amazon ip-ranges response

---
+++ rangoServices/src/main/java/com/company/aws/tools/rango/services/model/Ip4Prefix.java
@@ -0,0 +1,16 @@
+package com.company.aws.tools.rango.services.model;
+
+public class Ip4Prefix extends BasisPrefix {
+
+	private String ip_prefix;
+	
+	public String getIp_prefix() {
+		return ip_prefix;
+	}
+
+	public void setIp_prefix(String ip_prefix) {
+		this.ip_prefix = ip_prefix;
+	}
+
+	
+}
---
+++ rangoServices/src/main/java/com/company/aws/tools/rango/services/model/Ip6Prefix.java
@@ -0,0 +1,15 @@
+package com.company.aws.tools.rango.services.model;
+
+public class Ip6Prefix extends BasisPrefix {
+
+	private String ipv6_prefix;
+	
+	public String getIpv6_prefix() {
+		return ipv6_prefix;
+	}
+
+	public void setIpv6_prefix(String ipv6_prefix) {
+		this.ipv6_prefix = ipv6_prefix;
+	}
+
+}
