22c0afc03de559a303c12f94cd883e6d626529a6
jennycgonzalez <jenny.gonzalez@posteo.de>
2022-11-05 20:47:26 +0100

    ThrowIfNull test pass now

--- rangoServices/src/main/java/com/company/aws/tools/rango/services/utils/ParamChecker.java
+++ rangoServices/src/main/java/com/company/aws/tools/rango/services/utils/ParamChecker.java
@@ -1,11 +1,13 @@
 package com.company.aws.tools.rango.services.utils;
 
+import org.apache.commons.lang3.StringUtils;
+
 public final class ParamChecker {
 	
 	private ParamChecker() {}
 	
 	public static void throwIfBlank(String param, String message) {
-		if(param.isBlank()) {
+		if(StringUtils.isBlank(param)) {
 			throw new IllegalArgumentException(message);
 		}
 	}
