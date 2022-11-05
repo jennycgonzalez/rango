8e8d832bf823978da2b31e58be6659d8743585b4
jennycgonzalez <jenny.gonzalez@posteo.de>
2022-11-05 15:26:08 +0100

    complete test class annotations

--- rangoServices/src/test/java/com/company/aws/tools/rango/services/http/client/OkHttpClientTests.java
+++ rangoServices/src/test/java/com/company/aws/tools/rango/services/http/client/OkHttpClientTests.java
@@ -1,34 +1,32 @@
 package com.company.aws.tools.rango.services.http.client;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import okhttp3.Request;
-
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = {OkHttpClientService.class})
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class})
 class OkHttpClientTests {
 	
 	private final String IP_RANGES_URL = "https://ip-ranges.amazonaws.com/ip-ranges.json";
 	
+	@Autowired
 	OkHttpClientService client;
 
 	@Test
 	void getRequest_becomesSuccessfulResponse() {
-		Request request = buildGetRequest(IP_RANGES_URL);
+		assertNotNull(client);
+		HttpResponse response = client.get(IP_RANGES_URL);
 		
-		HttpResponse response = client.get(null);
-		
-		assertIsSuccessful(response);
+		assertRequestIsSuccessful(response);
 	}
 
-	private Request buildGetRequest(String url) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	private void assertIsSuccessful(HttpResponse response) {
+	private void assertRequestIsSuccessful(HttpResponse response) {
 		// TODO Auto-generated method stub
 		assertEquals(HttpResponseCode.REQUEST_SUCCESSFUL.getNumValue(), response.getStatusCode());
 	}
