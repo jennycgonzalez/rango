4ceba7f9bd8387cdd880823911a67c3f8f79ce92
jennycgonzalez <jenny.gonzalez@posteo.de>
2022-11-06 12:08:29 +0100

    First test for AmazonAWSClientService, which fails

--- rangoServices/src/main/java/com/company/aws/tools/rango/services/amazon/client/AmazonAWSClientService.java
+++ rangoServices/src/main/java/com/company/aws/tools/rango/services/amazon/client/AmazonAWSClientService.java
@@ -1,8 +1,22 @@
 package com.company.aws.tools.rango.services.amazon.client;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.company.aws.tools.rango.services.http.client.OkHttpClientService;
+import com.company.aws.tools.rango.services.model.IpRangesAmazon;
 
 @Service
 public class AmazonAWSClientService {
 
+	@Autowired
+	private OkHttpClientService httpClient;
+	
+	public IpRangesAmazon getIpRanges() {
+		// do get request
+		// parse the response body
+		// return IpRangesAmazon Object		
+		return null;
+	}
+	
 }
--- rangoServices/src/test/java/com/company/aws/tools/rango/services/amazon/client/AmazonAWSClientServiceTests.java
+++ rangoServices/src/test/java/com/company/aws/tools/rango/services/amazon/client/AmazonAWSClientServiceTests.java
@@ -1,14 +1,38 @@
 package com.company.aws.tools.rango.services.amazon.client;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-@SpringBootTest
+import com.company.aws.tools.rango.services.exceptions.AmazonAWSClientException;
+import com.company.aws.tools.rango.services.http.client.HttpResponse;
+import com.company.aws.tools.rango.services.http.client.OkHttpClientService;
+
+@SpringBootTest(classes = {AmazonAWSClientService.class, OkHttpClientService.class})
 public class AmazonAWSClientServiceTests {
+	
+	private HttpResponse EMPTY_RESPONSE = new HttpResponse();
+	
+	@MockBean
+	private OkHttpClientService httpClient;
 	
 	@Autowired
 	private AmazonAWSClientService amazonClient;
 	
-	
+	@Test
+	void getIpRanges_throwsException_whenResponseIsEmpty() {
+
+		when(httpClient.get(Mockito.anyString())).thenReturn(EMPTY_RESPONSE);
+		
+		assertThrows(AmazonAWSClientException.class, () -> {
+			amazonClient.getIpRanges();
+			
+		});
+	}
 
 }
