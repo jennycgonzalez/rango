015f75a3905d538e4e5ef7177762f7c7687f5fd4
jennycgonzalez <jenny.gonzalez@posteo.de>
2022-11-06 11:07:29 +0100

    commit okhhtpclient class this time... and the exception class

--- rangoServices/src/test/java/com/company/aws/tools/rango/services/http/client/OkHttpClientTests.java
+++ rangoServices/src/test/java/com/company/aws/tools/rango/services/http/client/OkHttpClientTests.java
@@ -3,17 +3,21 @@ package com.company.aws.tools.rango.services.http.client;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.company.aws.tools.rango.services.exceptions.OkHttpClientException;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = {OkHttpClientService.class})
 @EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class})
 class OkHttpClientTests {
 	
 	private final String IP_RANGES = "https://ip-ranges.amazonaws.com/ip-ranges.json";
+	private final String FAULTY_URL = "https://ip-ranges.amazonaws.com/ip-ranges-fake.json";
 	private final String EMPTY_URL = "";
 	
 	@Autowired
@@ -35,10 +39,14 @@ class OkHttpClientTests {
 			client.get(null);
 		});
 		
+		assertThrows(IllegalArgumentException.class, () -> {
+			client.get(null);
+		});
+		
 	}
 	
 	@Test
-	void get_receivesSuccessfulResponse() {
+	void get_returnsSuccessfulResponse_whenUrlIsCorrect() {
 		HttpResponse response = client.get(IP_RANGES);
 		
 		assertIsSuccessful(response);
@@ -48,4 +56,26 @@ class OkHttpClientTests {
 		assertEquals(HttpResponseCode.REQUEST_SUCCESSFUL.getNumValue(), response.getStatusCode());
 	}
 
+	@Test
+	void get_returnsForbiddenAsResponse_whenUrlIsFaulty() {
+		HttpResponse response = client.get(FAULTY_URL);
+		
+		assertIsFailure(response);
+	}
+	
+	private void assertIsFailure(HttpResponse response) {
+		assertEquals(HttpResponseCode.FORBIDDEN.getNumValue(), response.getStatusCode());
+	}
+	
+	@Disabled
+	@Test // For this test internet connection must be unavailable
+	void get_throwsException_whenThereIsNoInternetConnection() {
+		
+		assertThrows(OkHttpClientException.class, () -> {
+			client.get(IP_RANGES);
+		});
+		
+	}
+	
 }
+
