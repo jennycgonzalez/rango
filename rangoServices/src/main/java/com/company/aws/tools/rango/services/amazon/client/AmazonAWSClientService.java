1903a8b9e1de6196bef7d4b88b2c94f2bed6e239
jennycgonzalez <jenny.gonzalez@posteo.de>
2022-11-06 11:44:54 +0100

    Create AmazonAWSClientService and Test classes

---
+++ rangoServices/src/main/java/com/company/aws/tools/rango/services/amazon/client/AmazonAWSClientService.java
@@ -0,0 +1,8 @@
+package com.company.aws.tools.rango.services.amazon.client;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AmazonAWSClientService {
+
+}
---
+++ rangoServices/src/test/java/com/company/aws/tools/rango/services/amazon/client/AmazonAWSClientServiceTests.java
@@ -0,0 +1,14 @@
+package com.company.aws.tools.rango.services.amazon.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AmazonAWSClientServiceTests {
+	
+	@Autowired
+	private AmazonAWSClientService amazonClient;
+	
+	
+
+}
